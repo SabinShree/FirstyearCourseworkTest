957a6825c211d5d0d02a3922f9e8039521bdb916
SabinKharel <kharelsabin467@gmail.com>
2018-06-20 10:59:53 +0545

    Update First.py

--- First.py
+++ First.py
@@ -1,9 +1,9 @@
 def order():
-    print("Available products are:")
-    file = open("Stock.txt", "r")
-    s = file.readlines()
-    file.close()
-    l = []
-    for each in s:
-        l.append(each.replace("\n", "").split(","))
-    return l
+    my_list = []
+    with open("Stock.txt", "r") as my_file:
+        new_file = my_file.readlines()
+        for line in new_file:
+            my_list.append(line.strip("\n").split(","))
+    return my_list
+
+# print(order())
